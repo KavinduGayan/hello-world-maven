fd91c667e719fe237444cd0de9398172b1076e28
ECL\kavindu <kavindu@echannelling.com>
2018-12-09 19:00:53 +0530

    fix jenkins warning-2

--- src/main/java/com.kavindu.helloworld/Test.java
+++ src/main/java/com.kavindu.helloworld/Test.java
@@ -3,12 +3,16 @@ package com.kavindu.helloworld;
  * Created by Kavindu on 12/9/2018.
  */
 public final class Test {
+    /** {@inheritDoc} */
     private Test() {
     }
+
+    /** {@inheritDoc} */
     public static void main(final String[] args) {
         printThis("HI check jenkins checkStyle");
     }
 
+    /** {@inheritDoc} */
     public static void printThis(final String a) {
         System.out.println(a);
     }
---
+++ src/main/java/com.kavindu.helloworld/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Created by Kavindu on 12/9/2018.
+ */
+package com.kavindu.helloworld;
