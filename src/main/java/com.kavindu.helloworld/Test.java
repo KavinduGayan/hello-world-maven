3541512ec482b9eb57a0cdbacb60cb1d73ab8253
ECL\kavindu <kavindu@echannelling.com>
2018-12-09 18:53:18 +0530

    fix check style warning

--- src/main/java/com.kavindu.helloworld/Test.java
+++ src/main/java/com.kavindu.helloworld/Test.java
@@ -2,12 +2,14 @@ package com.kavindu.helloworld;
 /**
  * Created by Kavindu on 12/9/2018.
  */
-public class Test {
-    public static void main(String[] args) {
+public final class Test {
+    private Test() {
+    }
+    public static void main(final String[] args) {
         printThis("HI check jenkins checkStyle");
     }
 
-    public static void printThis(String a) {
+    public static void printThis(final String a) {
         System.out.println(a);
     }
 }
