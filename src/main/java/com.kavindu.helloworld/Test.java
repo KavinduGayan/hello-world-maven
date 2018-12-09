c5717f028225f8a556f7d14413044b5a8f6e10b6
ECL\kavindu <kavindu@echannelling.com>
2018-12-09 18:33:23 +0530

    add test class

---
+++ src/main/java/com.kavindu.helloworld/Test.java
@@ -0,0 +1,13 @@
+package com.kavindu.helloworld;
+/**
+ * Created by Kavindu on 12/9/2018.
+ */
+public class Test {
+    public static void main(String[] args) {
+        printThis("HI check jenkins checkStyle");
+    }
+
+    public static void printThis(String a) {
+        System.out.println(a);
+    }
+}
